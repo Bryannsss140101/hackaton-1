d4e190ff35d55f189b0f49694a041700deff2655
villegasarcecarlos@gmail.com <villegasarcecarlos@gmail.com>
2025-10-21 19:57:25 -0500

    Fix

--- src/main/java/org/idea/hackaton1/sale/application/SaleController.java
+++ src/main/java/org/idea/hackaton1/sale/application/SaleController.java
@@ -2,6 +2,7 @@ package org.idea.hackaton1.sale.application;
 
 import lombok.RequiredArgsConstructor;
 import org.idea.hackaton1.sale.domain.SaleService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/reports")
 public class SaleController {
     private final SaleService saleService;
+
+    
+
+
 }
