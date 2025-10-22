c2c838313da436736bcd194bc305b9e4677ed652
Bryannsss <bninamango.personal@gmail.com>
2025-10-21 19:23:56 -0500

    feat: add email

--- src/main/java/org/idea/hackaton1/Hackaton1Application.java
+++ src/main/java/org/idea/hackaton1/Hackaton1Application.java
@@ -2,8 +2,10 @@ package org.idea.hackaton1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Hackaton1Application {
 
     public static void main(String[] args) {
