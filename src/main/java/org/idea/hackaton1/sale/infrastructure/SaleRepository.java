2ed802a1b6ae522dec3a4c450595f98e3a75f074
Bryannsss <bninamango.personal@gmail.com>
2025-10-21 20:07:20 -0500

    feat

--- src/main/java/org/idea/hackaton1/sale/infrastructure/SaleRepository.java
+++ src/main/java/org/idea/hackaton1/sale/infrastructure/SaleRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    @Query("""
+                SELECT s FROM Sale s
+                WHERE (:branch IS NULL OR s.branch = :branch)
+                AND (:from IS NULL OR s.soldAt >= :from)
+                AND (:to IS NULL OR s.soldAt <= :to)
+            """)
     Page<Object> findByFilter(String branch, LocalDateTime from, LocalDateTime to, PageRequest pageRequest);
 }
